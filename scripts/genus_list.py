6ed4eb9af9e5e6c5262c95b8ff942ed69c8eb714
Cátia Baptista <s213049@dtu.dk>
2024-02-07 10:58:10 +0100

    new folders

---
+++ scripts/genus_list.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+taxa = pd.read_csv("/home/projects/animalia_mito/tools/taxa.tab", sep="\t", header = None, error_bad_lines=False, names=['a', 'b', 'name', 'c', 'rank', 'taxid'])
+
+path = "/home/biokate/thesis_unix/test_data/scripts/"
+
+chlorophyta_genus = []
+for index, row in taxa.iterrows():
+    if row['rank'] == 'genus' and '3041' in row['taxid']:
+        chlorophyta_genus.append(row['name'])
+        
+print(len(chlorophyta_genus))
+chloro = open((path + "chloro_genus"), "w")
+for e in chloro:
+    chloro.write(e + "\n")
+chloro.close()
