50c82eeaed7ea54d619e18382a6f2bc6a2870333
Cátia Baptista <s213049@dtu.dk>
2024-02-21 13:58:46 +0100

    code update
    downloads data and orders it by genus, family or order depending on the amount of sequences. Snakmake needs to be updated

--- scripts/download_fasta.py
+++ scripts/download_fasta.py
@@ -6,24 +6,18 @@ import gzip
 Entrez.email = 'catiacarmobatista@gmail.com'
 Entrez.api_key = '03d4de8e8f44ed94183a4ba257fab9752709'
 
-def search_genomes(genus_name, database='nuccore'):
-    #search_term = f"{genus_name}[Orgn]mitochondrion[Title] AND complete genome[Title] AND plants[filter]"
-    search_term = f"{genus_name}[Orgn]chloroplast[Title] AND complete genome[Title] AND plants[filter]"
+def search_genomes(search_term, database='nuccore'):
     id_list = []
-    
-    # Initial search to get count
     handle = Entrez.esearch(db=database, term=search_term, retmax=0)
     record = Entrez.read(handle)
     handle.close()
     count = int(record['Count'])
-    
-    batch_size = 500  # NCBI recommends not to exceed 500 for large queries
+    batch_size = 500
     for start in range(0, count, batch_size):
         handle = Entrez.esearch(db=database, term=search_term, retstart=start, retmax=batch_size)
         record = Entrez.read(handle)
         handle.close()
         id_list.extend(record['IdList'])
-    
     return id_list
 
 def fetch_genome_info(id_list, database='nuccore'):
@@ -31,43 +25,33 @@ def fetch_genome_info(id_list, database='nuccore'):
     records = SeqIO.parse(handle, "fasta")
     return list(records)
 
-def save_genome_sequences(records, directory="./"):
+def save_genome_sequences(records, directory):
+    id_species_mapping = []
     for record in records:
-        # Ensure the directory exists
         if not os.path.exists(directory):
             os.makedirs(directory)
-
-        # Construct the file path with .fasta.gz extension
         filepath = os.path.join(directory, f"{record.id}.fasta.gz")
-
-        # Write the FASTA file directly in gzip compressed format
         with gzip.open(filepath, "wt") as output_handle:
             SeqIO.write(record, output_handle, "fasta")
+        # Extract species name from the record description
+        species_name = ' '.join(record.description.split(' ')[1:3])  # Adjust as needed
+        id_species_mapping.append((record.id, species_name))
+    # Call the function to save ID-species mapping
+    save_id_species_mapping(id_species_mapping, directory)
 
+def save_id_species_mapping(id_species_mapping, directory):
+    filepath = os.path.join(directory, "id_species_mapping.txt")
+    with open(filepath, "w") as f:
+        for id, species in id_species_mapping:
+            f.write(f"{id}\t{species}\n")
 
-def read_genus_names_from_file(file_path):
-    with open(file_path, 'r') as file:
-        return [line.strip() for line in file if line.strip()]
-
-def main(file_path):
-    genus_names = read_genus_names_from_file(file_path)
-    for genus_name in genus_names:
-        # print(f"Searching for genomes of {genus_name}...")
-        ids = search_genomes(genus_name)
-        if len(ids) >= 5:
-            # output_dir = "data/mitochondrion/"+f"{genus_name}"
-            output_dir = "data/chloroplast/"+f"{genus_name}"
-            if not os.path.exists(output_dir):
-                os.makedirs(output_dir)
-            # print(f"Fetching genome information for {len(ids)} genomes...")
-            records = fetch_genome_info(ids)
-            # print(f"Saving genome sequences and taxon information...")
-            save_genome_sequences(records, directory=output_dir)
-            # print(f"Done. Data saved to {output_dir}")
-
+def main(search_term, output_dir):
+    ids = search_genomes(search_term)
+    if ids:
+        records = fetch_genome_info(ids)
+        save_genome_sequences(records, output_dir)
 
 if __name__ == "__main__":
-    #file_path = 'data/viridiplantae_genus_list.tsv'  
-    file_path = 'data/test_genus_list.tsv'
-    main(file_path)
-
+    search_term = "plants[filter] AND refseq[filter] AND chloroplast[filter] AND complete genome[Title]"
+    output_dir = "data/chloroplast"
+    main(search_term, output_dir)
