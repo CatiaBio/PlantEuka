de9a5cd93e65fdb8fb157f816d9cf147974a1976
Cátia Baptista <catiacarmobatista@gmail.com>
2024-06-30 12:28:41 +0200

    Update

--- scripts/merge_genomes.sh
+++ scripts/merge_genomes.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-# Description: 
-# This script merges FASTA files within subdirectories of a base directory.
-
 # Check if the correct number of arguments are passed
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <organelle>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <base_directory> <merged_directory>"
     exit 1
 fi
 
-# Assign command-line argument to a variable
-organelle="$1"
+# Assign command-line arguments to variables
+base_dir="$1"
+merged_dir="$2"
 
-# Define base and merged directories based on the organelle
-base_dir="${organelle}/genomes/sorted"
-merged_dir="${organelle}/genomes/merged"
-
-# Ensure the merged directory exists
+# Ensure the merged directory and its subdirectory 'acc_numb' exist
 mkdir -p "$merged_dir"
+mkdir -p "${merged_dir}/acc_numb"  # Create 'acc_numb' subdirectory
 
 # Function to process folders and merge FASTA files
 process_folder() {
@@ -30,18 +25,23 @@ process_folder() {
         return
     fi
 
-    # Output file name
+    # Output file names
     local output_file="${merged_dir}/${category}_${sub_dir_name}.fasta.gz"
+    # Modify tsv_file path to save inside the 'acc_numb' folder
+    local tsv_file="${merged_dir}/acc_numb/${category}_${sub_dir_name}.tsv"  # Adjusted path
 
     # Find and merge FASTA files
     find "$folder" -type f -name "*.fasta.gz" > /dev/null
     if [ $? -eq 0 ]; then
         find "$folder" -type f -name "*.fasta.gz" -print0 | xargs -0 zcat | gzip > "$output_file"
+        # Extract acc_numb after confirming file creation
+        zcat "$output_file" | grep "^>" | sed 's/>//g' | cut -d ' ' -f 1 > "$tsv_file"
     fi
 }
 
 # Categories to process
-categories=("genus" "family" "order")
+categories=("genus")
+#categories=("genus" "family" "order")
 
 # Loop through each category
 for category in "${categories[@]}"; do
